b274cdefd2cb970e982e45872e9c429a78f034c2
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2021-12-05 14:25:12 +0100

    #1: Prototype switch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Switch from './components/Switch';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Switch</h1>
+      <Switch
+        isOn={value}
+        //onColor="#EF476F"
+        onColor="#47ef87"
+        handleToggle={() => setValue(!value)}
+      />
     </div>
   );
 }
